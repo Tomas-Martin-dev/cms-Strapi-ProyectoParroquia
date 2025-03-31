e4f83f7b12a79f6bc38e11f0e7dffcaeeaf3a7cc
Tomas Martin <martintomas898@gmail.com>
2025-03-31 11:10:51 -0300

    actualizo middleware para visualizar las imagenes cargadas en strapi

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,7 +1,6 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
@@ -9,4 +8,19 @@ module.exports = [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'","https:"],
+          "img-src": ["'self'", "data:", "blob:", "https://res.cloudinary.com"],
+          "media-src": ["'self'", "data:", "blob:"],
+          upgradeInsecureRequests: null,
+        }
+      }
+    }
+  
+  }
 ];
